69675934bb33b64024bbee3cfc39c50489d5b6d0
Candlejack <garouspeak@gmail.com>
2022-08-27 14:10:38 +0300

    exercise 1 is donew

--- Exercise_1/exercise_1.cs
+++ Exercise_1/exercise_1.cs
@@ -1,1 +1,21 @@
-﻿
+﻿//  Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+using System;
+{
+    int [] array = new int [123];
+    Random rand = new Random();
+    Console.WriteLine("Ваш массив: ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, 1000);
+        Console.Write(array[i] + " ");
+    }
+    int count = 0;
+    foreach(int j in array)
+    {
+        if (j >= 10 & j <= 99)
+        count++;
+    }
+    Console.WriteLine("");
+    Console.WriteLine("Количество элементов массива в отрезке [10, 99] - " + count);
+}
